32d746064ab6a765a41de53b01bb05d1757b4e47
dennis89 <devilcool1985@gmail.com>
2021-04-15 21:22:47 +0800

    2021/08/10

--- .ipynb_checkpoints/request test-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url ='http://https://www.ptt.cc/bbs/Gossiping/index.html/'
-#
-res = requests.get(url)
-res.status_code
-# print(res)
--- request test.py
+++
@@ -1,9 +0,0 @@
-import requests
-payload = {
-'from': '/bbs/Gossiping/index.html',
-'yes': 'yes'
-}
-rs = requests.session()
-res = rs.post('https://www.ptt.cc/ask/over18',data=payload)
-res = rs.get('https://www.ptt.cc/bbs/Gossiping/index.html')
-print(res.text)
